dcd2294d3db98d6fac6df7f52346c0af3c1d22e2
Shivani <shivanikumari9979@gmail.com>
2025-12-13 21:02:29 +0530

    Solution added

---
+++ Day13/dec13a.cpp
@@ -0,0 +1,20 @@
+/*LEETCODE PASCAL TRIANGLE*/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        vector<int>tem(1,1);
+        ans.push_back(tem);
+        for(int i=1;i<numRows;i++){
+            vector<int>temp;
+            temp.push_back(1);
+            for(int j=0;j<i-1;j++){
+                temp.push_back(ans[i-1][j] + ans[i-1][j+1]);
+            }
+            temp.push_back(1);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
